884bc3f1d9c48977f107240455e7cce4697fef6d
francisco passarelli <118229728+franciscopassarelli@users.noreply.github.com>
2025-04-18 02:13:08 -0300

    first-commit

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  resolve: {
+  resolve: { 
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
